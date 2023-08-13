f7d1b557aafadffdc249ad0cc5e0de55f250ca17
Botir Tursunmurodov <lycr0343@gmail.com>
2023-08-14 00:16:39 +0500

    some changes

--- app/src/main/java/uz/lazydevv/zbekztask/presentation/extensions/ImageView.kt
+++ app/src/main/java/uz/lazydevv/zbekztask/presentation/extensions/ImageView.kt
@@ -2,7 +2,6 @@ package uz.lazydevv.zbekztask.presentation.extensions
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 import uz.lazydevv.zbekztask.R
 
 fun ImageView.loadImageUrl(url: String?) {
