788c0598b743855f219a0e047c524ed8b5d2d2c0
Botir Tursunmurodov <lycr0343@gmail.com>
2023-08-14 12:43:55 +0500

    some optimization

--- app/src/main/java/uz/lazydevv/zbekztask/presentation/ui/singlelesson/SingleLessonViewModel.kt
+++ app/src/main/java/uz/lazydevv/zbekztask/presentation/ui/singlelesson/SingleLessonViewModel.kt
@@ -6,7 +6,6 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
